69534c2e51d5a12e19fabfb688b8cf30cd168a02
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-22 15:26:44 +0200

    docs: assign class properties to constructor function

--- main.js
+++ main.js
@@ -25,6 +25,10 @@ class Veicolo {
     carburante;
 
     constructor (marca, anno, colore, porte, carburante) {
-        
+        this.marca = marca;
+        this.anno = anno;
+        this.colore = colore;
+        this.porte = porte;
+        this.carburante = carburante;
     }
 }
